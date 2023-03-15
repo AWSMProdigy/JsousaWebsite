81c982910762861df9abef788c932597a7d36ca9
AWSMProdigy <StraightAKyle@gmail.com>
2023-03-15 15:11:35 -0400

    Server side routing established

---
+++ client/server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static(path.join(__dirname, "..", "build")));
+app.use(express.static("public"));
+//Set the port that you want the server to run on
+const port = process.env.PORT || 8080;
+
+app.use((req, res, next) => {
+    res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+  });
+
+app.listen(port);
+console.log('App is listening on port ' + port);
